afe016a9806182df3f9e9e8e089fc32e800f31e8
Gagan Patle <gaganpatle7886@gmail.com>
2023-07-27 12:45:50 +0530

    forms

---
+++ action_page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta nsme="viewport" content="width=device-width, initial-scale=1.0>
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>I am action page</h2>
+    First Name is=<?php echo $_POST['fname']?><br>
+    Last Name is=<?php echo $_POST['lname']?><br>
+
+</body>
+</html>
+
